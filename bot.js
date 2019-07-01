1bb1a0d39ea57e2cc611f17fd57bc024bc47dc8c
KevinCalvez <51789767+KevinCalvez@users.noreply.github.com>
2019-07-01 17:11:31 +0200

    curl qui marche local a moitié

--- bot.js
+++ bot.js
@@ -9,7 +9,31 @@ class EchoBot extends ActivityHandler {
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
             if (context.activity.text === 'ticket') {
-                await context.sendActivity('Ticket?');
+                let res;
+                let request = require('request');
+                // eslint-disable-next-line handle-callback-err
+                request('http://127.0.0.1/kassandra/web/api/nbticket', async function(error, response, body) {
+                    console.log(body);
+                    res = body;
+                    await context.sendActivity(res);
+                });
+                await context.sendActivity(res);
+                // let options = {
+                //     host: 'http://localhost/web',
+                //     path: '/api/nbticket',
+                //     method: 'GET'
+                // };
+                // let req = http.request(options, function(res) {
+                //     console.log('STATUS: ' + res.statusCode);
+                //     console.log('HEADERS: ' + JSON.stringify(res.headers));
+                //     res.setEncoding('utf8');
+                //     res.on('data', function(chunk) {
+                //         console.log('BODY: ' + chunk);
+                //     });
+                //     req.on('error', function(e) {
+                //         console.log('problem with request: ' + e.message);
+                //     });
+                // });
             } else {
                 await context.sendActivity(`You said ME HAIL '${ context.activity.text }'`);
             }
@@ -21,7 +45,7 @@ class EchoBot extends ActivityHandler {
             const membersAdded = context.activity.membersAdded;
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity('Hello and welcome! Newcommer');
+                    await context.sendActivity('Hello and welcome!');
                 }
             }
             // By calling next() you ensure that the next BotHandler is run.
