95304e461f634b2fac5228ebb152bd10679d0f81
KevinCalvez <51789767+KevinCalvez@users.noreply.github.com>
2019-07-01 14:56:07 +0200

    Test curl

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+import * as http from 'node/http';
+
 const { ActivityHandler } = require('botbuilder');
 
 class EchoBot extends ActivityHandler {
@@ -9,9 +11,13 @@ class EchoBot extends ActivityHandler {
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
             if (context.activity.text === 'ticket') {
-                await context.sendActivity('Ticket?');
+                const options = new URL('http://localhost/kassandra/web/api/nbticket');
+
+                const req = http.request(options, (res) => {
+                });
+                await context.sendActivity(req);
             } else {
-                await context.sendActivity(`You said ME HAIL '${ context.activity.text }'`);
+                await context.sendActivity(`Je ne connais pas cette commande'`);
             }
             // By calling next() you ensure that the next BotHandler is run.
             await next();
