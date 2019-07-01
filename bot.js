24bf4695a5b878bfbb82c1fee6296399d714c495
KevinCalvez <51789767+KevinCalvez@users.noreply.github.com>
2019-07-01 15:43:51 +0200

    URL

--- bot.js
+++ bot.js
@@ -1,21 +1,14 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import * as http from 'node/http';
-
 const { ActivityHandler } = require('botbuilder');
 
 class EchoBot extends ActivityHandler {
     constructor() {
         super();
-        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
             if (context.activity.text === 'ticket') {
-                const options = new URL('http://localhost/kassandra/web/api/nbticket');
-
-                const req = http.request(options, (res) => {
-                });
-                await context.sendActivity(req);
+                await context.sendActivity('ok');
             } else {
                 await context.sendActivity(`Je ne connais pas cette commande'`);
             }
