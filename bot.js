47853417fac438d2754050a80a51fc39160681d5
KevinCalvez <51789767+KevinCalvez@users.noreply.github.com>
2019-07-01 12:53:03 +0200

    Add if

--- bot.js
+++ bot.js
@@ -8,8 +8,11 @@ class EchoBot extends ActivityHandler {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            await context.sendActivity(`You said ME HAIL '${ context.activity.text }'`);
-
+            if (context.activity.text === 'ticket') {
+                await context.sendActivity('Ticket?');
+            } else {
+                await context.sendActivity(`You said ME HAIL '${ context.activity.text }'`);
+            }
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
